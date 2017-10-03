b4fc6a9ea5b7edf4ee717ef750e42f7078589e92
Erick <erick_aug@hotmail.com>
2017-10-03 16:33:36 -0300

    Atualizar peso

--- src/BFS/MST.java
+++ src/BFS/MST.java
@@ -28,6 +28,27 @@ public class MST {
         int p = Constantes.INFINITO;
         g.vertices[id].chave = 0;
         FilaDePrioridade f = new FilaDePrioridade(g.vertices);
+        
+        while(!f.filaVazia()){
+            Vertice u = f.extraiMinimo();
+            id = u.getIndice();
+            for(i=0;i<g.listasDeAdjacencia[id].size();i++){
+                Arco a = (Arco) g.listasDeAdjacencia[id].get(i);
+                Vertice v = a.getDestino();
+                int peso = a.getPeso();
+                if(f.estaNaFila(v) && peso < v.chave){
+                    id = v.getIndice();
+                    g.vertices[id].chave = peso;
+                    g.vertices[id].pai = u;
+                    //Atualizar a fila! É onde está o problema para calcular os pesos
+                }
+                if(p > peso){
+                    p = peso;
+                }
+            }
+            w = w + p;
+        }
+        return w;
     }
     
 }
