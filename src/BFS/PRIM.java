7bca0cdf2d98d48acd6e95b6c30b7a1009d6175e
Erick <erick_aug@hotmail.com>
2017-10-03 16:09:13 -0300

    Retorna o Vérticce Mínimo

--- src/BFS/PRIM.java
+++ src/BFS/PRIM.java
@@ -23,6 +23,26 @@ public class PRIM {
         int p = Constantes.INFINITO;
         g.vertices[id].chave = 0;
         FilaDePrioridade f = new FilaDePrioridade(g.vertices);
-
+               
+        while(!f.filaVazia()){
+            Vertice u = f.extraiMinimo();
+            id = u.getIndice();
+            w = w + u.chave;
+            for(i=0;i<g.listasDeAdjacencia[id].size();i++){
+                Arco a = (Arco) g.listasDeAdjacencia[id].get(i);
+                Vertice v = a.getDestino();
+                int peso = a.getPeso();
+                if(f.estaNaFila(v) && peso < v.chave){
+                    int index = v.getIndice();
+                    //Atualiza no objeto fila
+                    f.fila[index].chave = peso;
+                    f.fila[index].pai = u;
+                }
+                if(p > peso){
+                    p = peso;
+                }
+            }
+        }
+        return w;
     }
 }
