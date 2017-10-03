c7b87d29ca8fd3e2acd890462c0430c736e67867
Erick <erick_aug@hotmail.com>
2017-10-03 14:59:24 -0300

    MST

---
+++ src/BFS/MST.java
@@ -0,0 +1,12 @@
+
+package BFS;
+
+public class MST {
+
+    public static void main(String[] args) {
+        //Leitura do teclado
+        GrafoPonderado g = new GrafoPonderado();
+        g.leDoTeclado();
+        //g.imprimeNaTela();zz
+    }
+}
