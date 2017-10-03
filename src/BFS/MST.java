36889d3a6310cffeea88063dd49d5d5cc2162650
Erick <erick_aug@hotmail.com>
2017-10-03 16:27:47 -0300

    Subrotina

--- src/BFS/MST.java
+++ src/BFS/MST.java
@@ -8,5 +8,26 @@ public class MST {
         GrafoPonderado g = new GrafoPonderado();
         g.leDoTeclado();
         //g.imprimeNaTela();zz
+        
+        //Parâmetros da leitura do teclado:
+        int n = g.totalDeVertices;
+        PRIM prim = new PRIM();
+        int p = prim.PRIM(g, 0, g.vertices[0]);
+        System.out.println("Peso: "+p);
     }
+    
+    public int PRIM(GrafoPonderado g, int w, Vertice s){
+        int i;
+                
+        for(i=0;i<g.totalDeVertices;i++){
+            g.vertices[i].chave = Constantes.INFINITO;
+            g.vertices[i].pai = null;
+        }
+        
+        int id = s.getIndice();
+        int p = Constantes.INFINITO;
+        g.vertices[id].chave = 0;
+        FilaDePrioridade f = new FilaDePrioridade(g.vertices);
+    }
+    
 }
