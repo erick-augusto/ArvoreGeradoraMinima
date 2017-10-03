339ef7820a07171bda6962ada4730ae09233fa5c
Erick <erick_aug@hotmail.com>
2017-10-03 15:31:23 -0300

    PRIM

---
+++ src/BFS/PRIM.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BFS;
+
+public class PRIM {
+    
+    PRIM(){
+        
+    }
+    
+    public int PRIM(GrafoPonderado g, int w, Vertice s){
+        int i;
+                
+        for(i=0;i<g.totalDeVertices;i++){
+            g.vertices[i].chave = Constantes.INFINITO;
+            g.vertices[i].pai = null;
+        }
+    }
+}
